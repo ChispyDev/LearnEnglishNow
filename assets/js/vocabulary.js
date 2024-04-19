0e975baf8cd70d12d77eef974042da22393adb69
ChispyDev <chispydev@gmail.com>
2024-04-19 11:02:50 +0200

    Vocabulary now fetch 100 random words

---
+++ assets/js/vocabulary.js
@@ -0,0 +1,39 @@
+/**
+ * @author Jose Antonio Lopez Perez - ChispyDev
+ */
+
+/**
+ * Possible random list size
+ */
+let wordsListCount = 100;
+/**
+ * Possible random words
+ */
+let wordsList = [];
+
+/**
+ * getPossibleWords method is used to fill the possible random words list
+ */
+async function getPossibleWords() {
+    wordsList = await getRandomWord(wordsListCount);
+    //console.log(wordsList);
+}
+
+async function getRandomWord(number) {
+    const endPoint = "https://random-word-api.herokuapp.com/word";
+    const endPointNumber = endPoint + "?number=" + number;
+    try {
+        const response = await fetch(endPointNumber);
+        if (!response.ok) {
+            console.log("Se ha producido un error al obtener la palabra");
+            return [];
+        }
+        const wordArray = await response.json();
+        return wordArray;
+    } catch (error) {
+        console.log("Se ha producido un error:", error);
+        return [];
+    }
+}
+
+getPossibleWords();
